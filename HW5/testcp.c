de58cf72b82771be5062a725d4dfe09c2200f606
peter0749 <peter0749@yahoo.com.tw>
2017-04-13 09:22:39 +0800

    fix warns

--- HW5/testcp.c
+++ HW5/testcp.c
@@ -23,7 +23,6 @@ int main(int argc, char **argv) {
     const char *helpArg="--help"; // 6 characters
     const char *tmpName="./tmpFile";
     int fileInFd=0, fileOutFd=0;
-    off_t fileSize=0;
     ssize_t inputSize=0, outputSize=0;
     const size_t BUFFER_SIZE=128*1024; // 128k
     char BUFFER[BUFFER_SIZE];
@@ -31,7 +30,7 @@ int main(int argc, char **argv) {
     struct stat statbuf;
     struct passwd *passwdEnt = getpwuid(getuid());
     char *home = passwdEnt->pw_dir;
-    if (argc !=3 || argc == 2 && strncmp(argv[1],helpArg,6)==0 ) {
+    if (argc !=3 || ( argc == 2 && strncmp(argv[1],helpArg,6)==0) ) {
         fprintf(stderr,"Usage: acp source_file target_file\n");
         exit(1);
     }
@@ -48,14 +47,13 @@ int main(int argc, char **argv) {
         perror("Error open output file");
         exit(3);
     }
-    fileSize = lseek(fileInFd, 0, SEEK_END);
     lseek(fileInFd, 0, SEEK_SET);
     lseek(fileOutFd, 0, SEEK_SET);
     while ( (inputSize = read(fileInFd, BUFFER, BUFFER_SIZE) ) > 0) {
         outputSize = write(fileOutFd, BUFFER, inputSize);
         if (outputSize != inputSize) {
             perror("input_size != output_size");
-            fprintf(stderr,"size: %d vs %d\n",inputSize, outputSize);
+            fprintf(stderr,"size: %d vs %d\n",(int)inputSize, (int)outputSize);
             exit(4);
         }
     }
